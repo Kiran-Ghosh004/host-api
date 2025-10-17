36facbff638a4503e93295fd8c17d660755df381
Kiran-Ghosh004 <kiranghosh520@gmail.com>
2025-10-18 01:03:42 +0530

    commit

--- db/connect.js
+++ db/connect.js
@@ -1,23 +1,11 @@
 const mongoose = require('mongoose');
 
-const uri = process.env.DB_URI; // Make sure DB_URI includes DB name
-
-let isConnected = false;
+const uri = `${process.env.DB_URI}/kiran-api`;
 
 const connectDB = async () => {
-  if (isConnected) return;
-
-  if (!uri) throw new Error('DB_URI not defined in environment variables');
-
-  try {
-    console.log('Connecting to MongoDB...');
-    await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-    isConnected = true;
-    console.log('MongoDB connected');
-  } catch (err) {
-    console.error('MongoDB connection error:', err);
-    throw err;
-  }
+    console.log("connect db");
+    
+    return mongoose.connect(uri)
 };
 
 module.exports = connectDB;
